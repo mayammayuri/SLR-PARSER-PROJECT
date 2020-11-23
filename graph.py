d46e831fe66b0a670b8236dec7d79f7b40314d75
jatin527 <jatinsingal2@gmail.com>
2020-11-23 23:32:39 +0530

    Graph Label update

--- graph.py
+++ graph.py
@@ -1,12 +1,7 @@
-import pydot
 from graphviz import *
-import pydotplus
-from pydot import Dot, Edge, Node
 
 graph = Digraph(format='png')
-#graph = Digraph()
-separator = "\n"
-file = open("F:\EBOOKS\Third year\Compiler design\lab\project\Items.txt", "r")
+file = open("Items.txt", "r")
 doc = file.readlines()
 il = int(doc[0][:-1])
 for i in range(il):
@@ -23,19 +18,30 @@ for i in range(il):
         ii = ii + (doc[j].replace("\n", "\\n"))
 
     iin = iin[:-2]
-    print(i,iin,ii)
     graph.node(iin, label=ii)
 
-# print(graph)
-edgesfile = open("F:\EBOOKS\Third year\Compiler design\lab\project\Edges.txt", "r")
+gotofile = open("Gotofile.txt", "r")
+edgesfile = open("Edges.txt", "r")
 edg = edgesfile.readlines()
+goto = gotofile.readlines()
+
 for x in edg:
     txt = x.split("|")
     edge1 = txt[0]
     edge2 = txt[1][:-1]
-    graph.edge(edge1, edge2)
-    # graph.edge(i)
-# graph.view()
-#render('dot', 'png', 'fname.dot')
+    ed = txt[1][1:]
+    e1 = edge1 + ":\n"
+    edgelabel = ""
+    if int(edge1[1:]) + 1 < il:
+        e2 = "I" + str(int(edge1[1:]) + 1) + ":\n"
+        i2 = goto.index(e2)
+    else:
+        i2 = len(goto)
+    i1 = goto.index(e1)
+    for y in range(i1+1, i2-1):
+        n, m = goto[y].split(",")
+        if m == ed:
+            edgelabel = n
+    graph.edge(edge1, edge2, label=edgelabel)
 
-graph.render('Graph', view=False)
+graph.render('Graph', view=True)
