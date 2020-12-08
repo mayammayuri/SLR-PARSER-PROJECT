6883cd1d9d59fa914393627952b0bfd1b1179023
unknown <mayurimane95923@gmail.com>
2020-12-08 14:51:47 +0530

    mayuri final

--- graph.py
+++ graph.py
@@ -1,47 +1,52 @@
 from graphviz import *
 
-graph = Digraph(format='png')
-file = open("Items.txt", "r")
-doc = file.readlines()
-il = int(doc[0][:-1])
-for i in range(il):
-    ii = ""
-    iin = 'I' + str(i) + ':\n'
-    ii0 = doc.index(iin)
-    if i+1 < il:
-        ii1 = 'I' + str(i+1) + ':\n'
-        ii1 = doc.index(ii1)
-    else:
-        ii1 = len(doc)
+def make_graph():
 
-    for j in range(ii0+1, ii1-1):
-        ii = ii + (doc[j].replace("\n", "\\n"))
+    graph = Digraph(format='png')
+    file = open("F:\EBOOKS\Third year\Compiler design\lab\project\Items.txt", "r")
+    doc = file.readlines()
+    il = int(doc[0][:-1])
+    print(il)
+    for i in range(il):
+        ii = ""
+        iin = 'I' + str(i) + ':\n'
+        ii0 = doc.index(iin)
+        if i+1 < il:
+            ii1 = 'I' + str(i+1) + ':\n'
+            ii1 = doc.index(ii1)
+        else:
+            ii1 = len(doc)
 
-    iin = iin[:-2]
-    graph.node(iin, label=ii)
+        for j in range(ii0+1, ii1-1):
+            ii = ii + (doc[j].replace("\n", "\\n"))
 
-gotofile = open("Gotofile.txt", "r")
-edgesfile = open("Edges.txt", "r")
-edg = edgesfile.readlines()
-goto = gotofile.readlines()
+        iin = iin[:-2]
+        graph.node(iin, label=ii)
 
-for x in edg:
-    txt = x.split("|")
-    edge1 = txt[0]
-    edge2 = txt[1][:-1]
-    ed = txt[1][1:]
-    e1 = edge1 + ":\n"
-    edgelabel = ""
-    if int(edge1[1:]) + 1 < il:
-        e2 = "I" + str(int(edge1[1:]) + 1) + ":\n"
-        i2 = goto.index(e2)
-    else:
-        i2 = len(goto)
-    i1 = goto.index(e1)
-    for y in range(i1+1, i2-1):
-        n, m = goto[y].split(" -> ")
-        if m == ed:
-            edgelabel = n
-    graph.edge(edge1, edge2, label=edgelabel)
+    gotofile = open("F:\EBOOKS\Third year\Compiler design\lab\project\Gotofile.txt", "r")
+    edgesfile = open("F:\EBOOKS\Third year\Compiler design\lab\project\Edges.txt", "r")
+    edg = edgesfile.readlines()
+    goto = gotofile.readlines()
 
-graph.render('Graph', view=True)
+    for x in edg:
+        txt = x.split("|")
+        edge1 = txt[0]
+        edge2 = txt[1][:-1]
+        ed = txt[1][1:]
+        e1 = edge1 + ":\n"
+        edgelabel = ""
+        if int(edge1[1:]) + 1 < il:
+            e2 = "I" + str(int(edge1[1:]) + 1) + ":\n"
+            i2 = goto.index(e2)
+        else:
+            i2 = len(goto)
+        i1 = goto.index(e1)
+        for y in range(i1+1, i2-1):
+            n, m = goto[y].split(" -> ")
+            if m == ed:
+                edgelabel = n
+        graph.edge(edge1, edge2, label=edgelabel)
+
+    graph.render('Graph', view=True)
+
+make_graph()
